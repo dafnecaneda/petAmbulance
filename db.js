64e1a6a94c585bc1024b96d3ddb357c4b8148f17
Daphne Nicole Caneda <daphne@Daphnes-MacBook-Air.local>
2022-02-21 23:14:45 -0300

    modifications for heroku

--- db.js
+++ db.js
@@ -3,12 +3,10 @@ const util = require("util");
 
 const pool = mysql.createPool({
 connectionLimit: 10,
-    host: 'localhost',
-    database: 'users',
-    user: 'root',
-    password: 'root',
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-    port: '8889'
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
 });
 
 pool.query = util.promisify(pool.query);
